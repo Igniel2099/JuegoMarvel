29c6ad2ff9f71f85651e81dbc49db8415012dbdf
Igniel <walthervallejo2018@gmial.com>
2025-04-22 22:00:10 +0200

    Agregar archivos de proyecto.

---
+++ JuegoMarvel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JuegoMarvel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JuegoMarvel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JuegoMarvel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
